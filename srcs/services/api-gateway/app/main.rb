9cf4851fded931ad158f36e03493f73f3fe769ea
Claudio Raimondi <claudio.raimondi@protonmail.com>
2024-10-18 19:44:05 +0200

    Add TODO

--- srcs/services/api-gateway/app/main.rb
+++ srcs/services/api-gateway/app/main.rb
@@ -3,21 +3,6 @@ require_relative 'EndpointTreeNode.rb'
 # Define the RESTful API endpoints
 EndpointTree = EndpointTreeNode.new('v1') # Root node
 
-EndpointTree.add_path('sessions',
-    [
-        ApiMethod.new(HttpMethod::GET, AuthLevel::ADMIN),   #get login status of all users
-        ApiMethod.new(HttpMethod::DELETE, AuthLevel::ADMIN) #disconnect all users
-    ]
-)
-EndpointTree.add_path('sessions/:user_id',
-    [
-        ApiMethod.new(HttpMethod::POST, AuthLevel::NONE),   #user login
-        ApiMethod.new(HttpMethod::GET, AuthLevel::USER),    #get user login status
-        ApiMethod.new(HttpMethod::DELETE, AuthLevel::USER)  #user logout
-    ]
-)
-
-EndpointTree.add_path('users',
-    [
+#TODO parse the API endpoints from a file and add them to the tree
 
 
