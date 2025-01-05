fc4c9850722c92323dbd759850cbdbdf7307067d
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-01-05 17:24:42 +0100

    Fix match paddle calculations

--- srcs/services/match_state/build/srcs/match.rb
+++ srcs/services/match_state/build/srcs/match.rb
@@ -6,17 +6,23 @@
 #    By: craimond <claudio.raimondi@pm.me>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/27 15:26:33 by craimond          #+#    #+#              #
-#    Updated: 2025/01/05 16:10:15 by craimond         ###   ########.fr        #
+#    Updated: 2025/01/05 17:24:19 by craimond         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-#TODO rinominare match into match
 
 require_relative 'config_handler'
 require_relative 'exceptions'
 
 class Match
   attr_reader :players, :state
+
+  CENTER = 0.5
+  COURT_BOUNDS = (0..1).freeze
+  VELOCITY_RANGE = (-0.5..0.5).freeze
+
+  PADDLE_LENGTH = 0.3
+  PADDLE_HALF_LENGTH = PADDLE_LENGTH / 2
+  PADDLE_RANGE = ((PADDLE_HALF_LENGTH)..(1 - PADDLE_HALF_LENGTH)).freeze
 
   @config = ConfigHandler.instance.config
 
@@ -25,10 +31,10 @@ class Match
   end
 
   STARTING_match_state = {
-    ball_position: Array.new(2, 0),
-    ball_velocity: Array.new(2, 0),
-    paddle_positions: Array.new(2, 0.5),
-    health_points: Array.new(2, max_hp),
+    ball_position: [0.5, 0.5],
+    ball_velocity: [],
+    paddle_positions: [0.5, 0.5],
+    health_points: [max_hp, max_hp],
     status: :waiting,
     timestamp: 0,
   }
@@ -40,6 +46,8 @@ class Match
     @state = deep_copy(STARTING_match_state)
     @input_queue = []
     @state_history = []
+
+    reset_ball
   end
 
   def add_player(user_id, ws)
@@ -58,7 +66,7 @@ class Match
 
   def surrender_player(user_id)
     idx = player_idx(user_id)
-    @status[:health_points][idx] = 0
+    @state[:health_points][idx] = 0
     @state[:status] = :ended
   end
 
@@ -70,108 +78,124 @@ class Match
     @input_queue << input
   end
 
-  #TODO refactor e ricontrollare in base a aas
-
-  # def update
-  #   return unless @status == :ongoing
+  def update
+    return unless @state[:status] == :ongoing
     
-  #   current_time = current_time_ms
-  #   process_inputs(current_time)
+    current_time = current_time_ms
+    process_inputs(current_time)
     
-  #   @state_history << deep_copy(@state)
-  #   @state_history.reject! { |s| current_time - s[:timestamp] > @config.dig(:server, :max_lag_compensation) }
+    @state_history << deep_copy(@state)
+    @state_history.reject! do |state|
+      current_time - state[:timestamp] > @config.dig(:server, :max_lag_compensation)
+    end
   
-  #   @state[:timestamp] = current_time
-  # end
+    @state[:timestamp] = current_time
+  end
 
-  # private
+  private
 
-  # def process_inputs
-  #   @input_queue.sort_by! { |input| input[:timestamp] }
-  #   @input_queue.each do |input|
-  #     apply_input(input, current_time)
-  #   end
-  #   @input_queue.clear
-  # end
+  def process_inputs(current_time)
+    @input_queue.sort_by! { |input| input[:timestamp] }
+    
+    @input_queue.each do |input|
+      apply_input(input, current_time)
+    end
 
-  # def apply_input(input, current_time)
-  #   user_id = input[:user_id]
-  #   client_time = input[:timestamp]
-  #   direction = input[:direction]
-  #   check_required_fields(user_id, client_time, direction)
-  #   raise GRPC::InvalidArgument.new('User not in match') unless @players.keys.include?(user_id)
-  #   raise GRPC::InvalidArgument.new('Invalid direction format') unless [-1, 0, 1].include?(direction)
-  #   raise GRPC::InvalidArgument.new('Invalid timestamp format') unless client_time.is_a?(Integer)
+    @input_queue.clear
+  end
 
-  #   server_delay = current_time - client_time
-  #   return if server_delay > @config.dig(:server, :max_lag_compensation)
+  def apply_input(input, current_time)
+    user_id = input[:user_id]
+    client_time = input[:timestamp]
+    direction = input[:direction]
+    check_required_fields(user_id, client_time, direction)
+    raise GRPC::InvalidArgument.new('User not in match') unless @players.keys.include?(user_id)
+    raise GRPC::InvalidArgument.new('Invalid direction format') unless [-1, 0, 1].include?(direction)
+    raise GRPC::InvalidArgument.new('Invalid timestamp format') unless client_time.is_a?(Integer)
+    return if direction.zero?
+
+    lag = current_time - client_time
+    return if lag > @config.dig(:server, :max_lag_compensation)
    
-  #   closest_state = find_closest_state(client_time)
-  #   temp_state = deep_copy(closest_state)
+    closest_state = find_closest_state(client_time)
+    temp_state = deep_copy(closest_state)
 
-  #   player_number = @players.keys.index(user_id)
-  #   player_key = player_number == 0 ? :player_1 : :player_2
+    player_idx = @players.keys.index(user_id)
 
-  #   new_paddle_position = temp_state[:paddle_positions][player_key]
-  #   case direction
-  #   when -1
-  #     new_paddle_position -= @config.dig(:settings, :paddle_speed)
-  #   when 1
-  #     new_paddle_position += @config.dig(:settings, :paddle_speed)
-  #   end
+    new_paddle_position = temp_state[:paddle_positions][player_idx]
+    case direction
+    when 
+      new_paddle_position -= @config.dig(:settings, :paddle_speed)
+    when 1
+      new_paddle_position += @config.dig(:settings, :paddle_speed)
+    end
 
-  #   new_paddle_position = clamp(new_paddle_position, 0, 1)
-  #   temp_state[:paddle_positions][player_key] = new_paddle_position
+    new_paddle_position.clamp!(PADDLE_RANGE.first, PADDLE_RANGE.last)
+    temp_state[:paddle_positions][player_idx] = new_paddle_position
 
-  #   replay_physics(temp_state, client_time, current_time)
+    replay_physics(temp_state, client_time, current_time)
 
-  #   @state[:paddle_positions][player_key] = temp_state[:paddle_positions][player_key]
-  #   @state[:ball_position] = temp_state[:ball_position]
-  #   @state[:ball_velocity] = temp_state[:ball_velocity]
-  # end
+    @state[:paddle_positions][player_idx] = temp_state[:paddle_positions][player_idx]
+    @state[:ball_position] = temp_state[:ball_position]
+    @state[:ball_velocity] = temp_state[:ball_velocity]
+  end
 
-  # def find_closest_state(timestamp)
-  #   @state_history.min_by { |state| (state[:timestamp] - timestamp).abs }
-  # end
+  def find_closest_state(timestamp)
+    @state_history.min_by { |state| (state[:timestamp] - timestamp).abs }
+  end
 
-  # def replay_physics(state, start_time, end_time)
-  #   delta_time = (end_time - start_time) / 1000.0
+  def replay_physics(state, start_time, end_time)
+    delta_time = (end_time - start_time) / 1000.0
     
-  #   state[:ball_position][:x] += state[:ball_velocity][:x] * delta_time
-  #   state[:ball_position][:y] += state[:ball_velocity][:y] * delta_time
+    state[:ball_position][0] += state[:ball_velocity][0] * delta_time
+    state[:ball_position][1] += state[:ball_velocity][1] * delta_time
     
-  #   if state[:ball_position][:y] <= -1 || state[:ball_position][:y] >= 1
-  #     state[:ball_velocity][:y] *= -1
-  #   end
+    unless COURT_BOUNDS.cover?(state[:ball_position][1])
+      state[:ball_velocity][1] *= -1
+    end
     
-  #   check_paddle_collisions(state)
-  # end
+    check_paddle_collisions(state)
+  end
 
-  # def check_paddle_collisions(state)
-  #   if state[:ball_position][:x] <= -0.9
-  #     if (state[:ball_position][:y] - state[:paddle_positions][:player_1]).abs < 0.1
-  #       state[:ball_velocity][:x] *= -1
-  #     else
-  #       lose_point(:player_1)
-  #   end
+  def check_paddle_collisions(state)
+    ball_x, ball_y = state[:ball_position]
+    left_paddle_center, right_paddle_center = state[:paddle_positions]
+  
+    if ball_x <= -PADDLE_BOUNDARY
+      check_collision(ball_y, left_paddle_center, 0, state)
+    elsif ball_x >= PADDLE_BOUNDARY
+      check_collision(ball_y, right_paddle_center, 1, state)
+    end
+  end
+  
+  private
+  
+  def check_collision(ball_y, paddle_center, player_idx, state)
+    paddle_min = paddle_center - PADDLE_HALF_LENGTH
+    paddle_max = paddle_center + PADDLE_HALF_LENGTH
     
-  #   elsif state[:ball_position][:x] >= 0.9
-  #     if (state[:ball_position][:y] - state[:paddle_positions][:player_2]).abs < 0.1
-  #       state[:ball_velocity][:x] *= -1
-  #     else
-  #       lose_point(:player_2)
-  #     end
-  #   end
-  # end
+    if ball_y.between?(paddle_min, paddle_max)
+      state[:ball_velocity][0] *= -1
+    else
+      lose_point(player_idx)
+    end
+  end
 
-  # def lose_point(player_key)
-  #   @state[:health_points][player_key] -= 1
-  #   if @state[:health_points][player_key] == 0
-  #     @state[:status] = :ended
-  #   end
-  #   @state[:ball_position] = { x: 0, y: 0 }
-  #   @state[:ball_velocity] = { x: random_velocity, y: random_velocity }
-  # end
+  def lose_point(player_idx)
+    @state[:health_points][player_idx] -= 1
+  
+    if @state[:health_points][player_idx] == 0
+      @state[:status] = :ended
+    else
+      reset_ball
+    end
+  end
+
+  def reset_ball
+    @state[:ball_position] = [0.5, 0.5]
+    velocity = [random_direction, random_direction]
+    @state[:ball_velocity] = velocity
+  end
 
   def player_idx(user_id)
     @players.keys.index(user_id)
@@ -187,9 +211,12 @@ class Match
     (Time.now.to_f * 1000).to_i
   end
 
-  # def random_velocity
-  #   rand(-0.5..0.5) * @config.dig(:settings, :ball_speed)
-  # end
+  def random_direction
+    speed = @config.dig(:settings, :ball_speed)
+    dir = rand(VELOCITY_RANGE) 
+    dir = rand(VELOCITY_RANGE) while dir.zero?
+    dir * speed
+  end
 
   def deep_copy(obj)
     Marshal.load(Marshal.dump(obj))
