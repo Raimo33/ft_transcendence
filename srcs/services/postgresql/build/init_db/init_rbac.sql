c8885ef6fd35d9710315adbdd13cba468a43a5d7
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-01-05 16:17:50 +0100

    Add pg rbac

--- srcs/services/postgresql/build/init_db/init_rbac.sql
+++ srcs/services/postgresql/build/init_db/init_rbac.sql
@@ -1,7 +1,7 @@
 CREATE USER app WITH PASSWORD 'password';
 CREATE USER match_state WITH PASSWORD 'password'
 
--- #TODO grant all to app
--- #TODO grant match, tournament, to match_state
+GRANT SELECT INSERT UPDATE DELETE ON ALL TABLES IN SCHEMA public TO app;
 
-GRANT 
+GRANT SELECT INSERT UPDATE DELETE ON Matches      TO match_state;
+GRANT SELECT INSERT UPDATE DELETE ON Tournaments  TO match_state;
